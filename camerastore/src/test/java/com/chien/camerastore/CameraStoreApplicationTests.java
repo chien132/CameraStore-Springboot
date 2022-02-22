ec449ee333fbaf34ff7af95cd8c50da6af3418c9
Chien Dang <44261143+chien132@users.noreply.github.com>
2022-02-23 02:28:39 +0700

    init and test

---
+++ camerastore/src/test/java/com/chien/camerastore/CameraStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chien.camerastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CameraStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
